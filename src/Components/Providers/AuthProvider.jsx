8fb0a02645935296825a9d4ff095fa1f34bfd105
Saint Lord <thesaint@Saints-MacBook-Pro.local>
2023-04-26 00:45:01 +0600

    Context api with AuthProvider created

---
+++ src/Components/Providers/AuthProvider.jsx
@@ -0,0 +1,14 @@
+import React, { createContext } from 'react';
+
+const AuthContext = createContext(null)
+
+const AuthProvider = ({children}) => {
+    const AuthInfo = {}
+    return (
+        <AuthContext.Provider value = {AuthInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.css'
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+
+    // Declare state for error
+    const [ error, setError ] = useState('');
+
+    // On submit click handler
+    const handleSignUp = (event) => {
+        event.preventDefault();
+
+        // Retrieve Form data
+        const form = event.target;
+        const email = form.email.value;
+        const pass = form.pass.value;
+        const confirmPass = form.confirmPass.value;
+        console.log(email, pass, confirmPass);
+
+        // Validations
+        if ( pass !== confirmPass ) {
+            setError('Password did not match');
+            return
+        } else if (pass.length < 6){
+            setError('Password must be at least 6 characters');
+            return
+        }
+    };
     return (
         <div className="formContainer">
       <h3 className="formTitle">Register</h3>
-      <form action="">
+      <form onSubmit={handleSignUp} action="">
         <div className="formControl">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="" required />
@@ -19,6 +43,7 @@ const SignUp = () => {
           <label htmlFor="confirmPass">Confirm Password</label>
           <input type="password" name="confirmPass" id="" required />
         </div>
+        <p className='errorMessage'>{error}</p>
         <button className="logInBtn" type="submit">
           Sign Up
         </button>
